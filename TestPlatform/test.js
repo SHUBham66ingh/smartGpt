cd569e711a9794e192cd5b64c2939144e1f56969
Shubham singh <shubham1041singh@gmail.com>
2026-08-07 13:33:12 +0530

    testDone

---
+++ TestPlatform/test.js
@@ -0,0 +1,18 @@
+import { OpenRouter } from '@openrouter/sdk';
+import "dotenv/config";
+
+const client = new OpenRouter({
+  apiKey: '<OPENROUTER_API_KEY>',
+});
+
+const completion = await client.chat.send({
+  model: '~openai/gpt-latest',
+  messages: [
+    {
+      role: 'user',
+      content: 'What is the meaning of life?',
+    },
+  ],
+});
+
+console.log(completion.choices[0].message.content);
